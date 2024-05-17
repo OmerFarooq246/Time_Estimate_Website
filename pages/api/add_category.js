33d550cfddab21f1831085e121162fd4b20eb13c
Omer Farooq <ofarooq.bscs21seecs@seecs.edu.pk>
2024-05-17 21:08:48 +0500

    add cat error fix

--- pages/api/add_category.js
+++ pages/api/add_category.js
@@ -3,18 +3,17 @@
 import prisma from "../../client"
 
 export default async function handler(req, res){
-    console.log("req.body in add_category: ", JSON.parse(req.body))
-    const data = JSON.parse(req.body)
+    console.log("req.body in add_category: ", req.body)
 
     try{
         const category = await prisma.categories.create({
             data: {
-                name: data.categoryData.name,
-                img_source: data.categoryData.img_source,
+                name: req.body.categoryData.name,
+                img_source: req.body.categoryData.img_source,
             }
         })
         console.log("category created: ", category)
-        res.status(200).json(JSON.stringify(category))
+        res.status(200).json(category)
     }
     catch(error){
         console.log("error in creating category: ", error)
